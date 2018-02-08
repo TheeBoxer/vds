d168d8153fc2918417b9050c821f1bbaf9a4d775
Nolan Holden <nolanholden@users.noreply.github.com>
2018-02-08 18:49:35 -0500

    add starting point for justin

--- linux/src/rcr/vds/vds.cc
+++ linux/src/rcr/vds/vds.cc
@@ -1,45 +1,34 @@
-#include <cpp_mediator/mediator.hpp>
-#include <rcr/sensors/vn/vectornav.hh>
-#include <rcr/beaglebone/io/spi/spi.hh>
-
-#include <memory>
+#include <chrono>
+#include <thread>
 
 #include <stdio.h>
 
-namespace rcr {
-namespace vds {
+// justin, put whatever you want here
 
-namespace sdata {
-  constexpr const char* const kFlightLogFilename = "/tmp/vds_flight_log";
-  constexpr const char* const kFileIoMode = "a";
-  holden::mediator m{};
-  auto vn_handler = std::make_shared<sensors::vn::VectorNavHandler>(m);
-  auto spi_handler = std::make_shared<rcr::beaglebone::io::spi::SpiHandler>(m);
+void test_motor() {
+  std::chrono::milliseconds half_second{ 500 };
 
-  FILE* flight_log = NULL;
-  sensors::vn::PrintAllStatus vn_request_all{ &flight_log, fprintf };
-} // namespace sdata
-} // namespace vds
-} // namespace rcr
+  std::this_thread::sleep_for(half_second);
+  // actuate to 1/4 full extend
+  // motor.open_to(EncoderPos::50);
 
-int main() {
-  printf("vds starting");
-  using namespace rcr::vds::sdata;
-  
-  m.register_handler(vn_handler);
-  m.register_handler(spi_handler);
-  m.send(rcr::beaglebone::io::spi::Open{});
-while(1) {
-  flight_log = NULL;
-  flight_log = fopen(kFlightLogFilename, kFileIoMode);
-  if (flight_log) {
-    fprintf(flight_log, "yo dawg\n");
-    m.send(vn_request_all);
-    fclose(flight_log);
-  } else {
-    printf("fatal error: \"/tmp/vds_flight_log\" could not open");
-  }
+  std::this_thread::sleep_for(half_second);
+  // actuate to 2/4 full extend
+  // motor.open_to(EncoderPos::100);
+
+  std::this_thread::sleep_for(half_second);
+  // actuate to 3/4 full extend
+  // motor.open_to(EncoderPos::150);
+
+  std::this_thread::sleep_for(half_second);
+  // actuate to full extend
+  // motor.open_to(EncoderPos::50);
 }
 
-  return 0;
+
+
+int main() {
+  while(1) {
+    test_motor();
+  }
 }
