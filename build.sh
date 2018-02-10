6ed8a3d067cb8fb8fbe7f41780ddeaecd2ec82e0
Nolan Holden <holden.nolan@gmail.com>
2018-02-10 14:29:16 -0500

    try explicit path

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ readonly BUILD_DIR='build/'
 mkdir ${BUILD_DIR}
 cd    ${BUILD_DIR}
 
-cmake ../linux/ -DCMAKE_TOOLCHAIN_FILE=../linux/arm-linux-gnueabihf.cmake
+cmake ../linux/ -DCMAKE_CXX_COMPILER=/usr/bin/arm-linux-gnueabihf-g++ -DCMAKE_CC_COMPILER=/usr/bin/arm-linux-gnueabihf-gcc
 make
