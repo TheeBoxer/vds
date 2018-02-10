4285c179c18a8a839b69816837f609461a79a96e
Nolan Holden <nolanholden@users.noreply.github.com>
2018-02-10 14:04:09 -0500

    use gcc 7

--- linux/arm-linux-gnueabihf.cmake
+++ linux/arm-linux-gnueabihf.cmake
@@ -3,8 +3,8 @@ set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSROOT /home/devel/rasp-pi-rootfs)
 set(CMAKE_STAGING_PREFIX /home/devel/stage)
 
-set(CMAKE_C_COMPILER   /usr/bin/arm-linux-gnueabihf-gcc)
-set(CMAKE_CXX_COMPILER /usr/bin/arm-linux-gnueabihf-g++)
+set(CMAKE_C_COMPILER   /usr/bin/arm-linux-gnueabihf-gcc-7)
+set(CMAKE_CXX_COMPILER /usr/bin/arm-linux-gnueabihf-g++-7)
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
