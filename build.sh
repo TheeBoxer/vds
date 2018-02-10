4790b1aa1d52d4a4ea6823507de810a90575d10a
Nolan Holden <nolanholden@users.noreply.github.com>
2018-02-10 16:02:25 -0500

    use local g++

--- build.sh
+++ build.sh
@@ -5,5 +5,6 @@ readonly BUILD_DIR='build/'
 mkdir ${BUILD_DIR}
 cd    ${BUILD_DIR}
 
-cmake ../linux/ -DCMAKE_CXX_COMPILER=/usr/bin/arm-linux-gnueabihf-g++ -DCMAKE_CC_COMPILER=/usr/bin/arm-linux-gnueabihf-gcc
+cmake ../linux/
 make
+
--- build_debug.sh
+++ build_debug.sh
@@ -6,6 +6,6 @@ mkdir ${DEBUG_BUILD_DIR}
 cd    ${DEBUG_BUILD_DIR}
 
 # debug
-cmake ../linux/ -DCMAKE_BUILD_TYPE=DEBUG -DCMAKE_TOOLCHAIN_FILE=../linux/arm-linux-gnueabihf.cmake
+cmake ../linux/ -DCMAKE_BUILD_TYPE=DEBUG
 make
 
